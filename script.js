5e6295e58837f2fcf420ef01d4e41f1f6d1b1776
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-01-28 10:23:22 -0300

    commit
    first exercise of the day

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+
 class Node {
-    constructor(vaue){
+    constructor(value){
         this.value = value;
         this.left = null;
         this.right = null;
@@ -12,10 +13,59 @@ class BinarySearchTree {
     }
     insert(value){
         let newNode = new Node(value);
-
-        if(!this.root) {
+        if(!this.root){
             this.root = newNode;
             return this;
         }
+        let current = this.root;
+
+        while(true){
+            if(value === current.value) return undefined;
+            if(value > current.value){
+                if(!current.right){
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }else {
+                if(!current.left){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            }
+        }
     }
+    find(value){
+        if(!this.root) return false;
+        let current = this.root, found = false;
+
+        while(current && !found){
+            if(value > current.value){
+                current = current.right;
+            }else if(value < current.value){
+                current = current.left;
+            }else {
+                found = true;
+            }
+        }
+        if(!found) return undefined
+        return current;
+    }
+    constains(value){
+        if(!this.root) return undefined;
+        let current = this.root, found = false;
+
+        while(current && !found){
+            if(value > current.value){
+                current = current.right;
+            }else if(value < current.value){
+                current = current.left;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }
