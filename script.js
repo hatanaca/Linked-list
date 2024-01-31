61fdc49f4a3d0f9e122310494b174b58024503b4
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-01-31 17:25:55 -0300

    Hash table code

--- script.js
+++ script.js
@@ -1,82 +1,35 @@
-class PriorityQueue {
-    constructor(){
-        this.values = [];
+class HashTable {
+    constructor(size=53){
+        this.keyMap = new Array(size);
     }
-    enqueue(val, priority){
-        let newNode = new Node(val, priority);
-        this.values.push(newNode);
-        this.bubbleUp();
-    }
-    bubbleUp(){
-        let idx = this.values.length - 1;
-        const element = this.values[idx];
-        while(idx > 0){
-            let parentIdx = Math.floor((idx - 1)/2);
-            let parent = this.values[parentIdx];
-            if(element.priority >= parent.priority) break;
-            this.values[parentIdx] = element;
-            this.values[idx] = parent;
-            idx = parentIdx;
-        }
-    }
-    dequeue(){
-        const min = this.values[0];
-        const end = this.values.pop();
-        if(this.values.length > 0){
-            this.values[0] = end;
-            this.sinkDown();
-        }
-        return min;
-    }
-    sinkDown(){
-        let idx = 0;
-        const element = this.values[0];
-        const length = this.values.length;
-        while(true){
-            let leftChildIdx = 2 * idx + 1;
-            let rightChildIdx = 2 * idx + 2;
-            let leftChild,rightChild;
-            let swap = null;
 
-            if(leftChildIdx < length){
-                leftChild = this.values[leftChildIdx];
-                if(leftChild.priority < element.priority) {
-                    swap = leftChildIdx;
+    _hash(key){
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for(let i = 0; i < Math.min(key, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+    set(key, value){
+        let index = this._hash(key);
+        if(!this.keyMap[index]){
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
+    get(key){
+        let index = this._hash(key);
+        if(this.keyMap[index]) {
+            for(let i = 0; i < this.keyMap[index].length; i++){
+                if(this.keyMap[index][i][0] === key) {
+                    return this.keyMap[index] [i][1]
                 }
             }
-            if(rightChildIdx < length){
-                rightChild = this.values[rightChildIdx];
-                if(
-                    (swap === null && rightChild.priority < element.priority) || 
-                    (swap !== null && rightChild.priority < leftChild.priority)
-                ) {
-                   swap = rightChildIdx;
-                }
-            }
-            if(swap === null) break;
-            this.values[idx] = this.values[swap];
-            this.values[swap] = element;
-            idx = swap;
         }
+        return undefined;
     }
 }
-
-class Node {
-    constructor(val, priority){
-        this.val = val;
-        this.priority = priority;
-    }
-}
-
-let ER = new PriorityQueue();
-ER.enqueue("common cold",5)
-ER.enqueue("gunshot wound", 1)
-ER.enqueue("high fever",4)
-ER.enqueue("broken arm",2)
-ER.enqueue("glass in foot",3)
-
-
-
-
-
-
