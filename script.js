17c1ba5fc9365842b75a530e7b4e242895ceaa85
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-02 14:07:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,13 @@
-class Graph{
-    constructor(){
-        this.adjacencyList = {};
-    }
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    }
-    addEdge(v1, v2){
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push	(v1);
-    }
-    removeEdge(vertex1, vertex2){
-        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-            v => v !== vertex2
-        );
-        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-            v => v !== vertex2
-        );
-    }
-    removeVertex(vertex){
-        while(this.adjacencyList[vertex].length){
-            const adjacencyEdge = this.adjacencyList[vertex].pop();
-            this.removeEdge(vertex, adjacencyEdge);
-        }
-        delete this.adjacencyList[vertex];
-    }
+function binarySearch(arr, elem){
+    let start = 0;
+    let end = arr.length;
+    let middle = Math.floor((start + end)/ 2)
 
-    depthFirstRecursive(start){
-        const result = [];
-        const visited = {};
-        const adjacencyList = this.adjacencyList;
-
-        (function dfs(vertex){
-            if(!vertex) return null;
-            visited[vertex] = true;
-            result.push(vertex);
-            adjacencyList[vertex].forEach(neighbor => {
-                if(!visited[neighbor]){return dfs(neighbor)}
-            });
-        })(start);
+    while(arr[middle]!==elem && start <= end){
+        if(elem < arr[middle]) end = middle -1;
+        else start = middle + 1;
+        middle = Math.floor((start + end)/2);
+        if(arr[middle] === elem) return middle;
+        return -1;
     }
-    depthFirstIterative(start){
-        const stack = [start];
-        const result = [];
-        const visited = {};
-        let currentVertex;
-        visited[start] = true;
-
-        while(stack.length){
-            currentVertex = stack.pop();
-            result.push(currentVertex);
-
-            this.adjacencyList[currentVertex].forEach(neighbor =>{
-                if(!visited[neighbor]){
-                    visited[neighbor] = true;
-                    stack.push(neighbor);
-                }
-            });
-        }
-        return result;
-    }
-    breadthFirst(start){
-        const queue =[start];
-        const result = [];
-        const visited = {};
-        let currentVertex;
-        visited[start] = true;
-
-        while(queue.length){
-            currentVertex = queue.shift();
-            result.push(currentVertex);
-
-            this.adjacencyList[currentVertex].forEach(neighbor =>{
-                if(!visited[neighbor]){
-                    visited[neighbor] = true;
-                    queue.push(neighbor);
-                }
-            });
-        }
-        return result;
-    }
-
 }
