9b3fc26dcb48876c56d68e06dfc8fdbca0ebf509
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-02 14:13:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function binarySearch(arr, elem){
+function binarySearch(arr,elem){
     let start = 0;
-    let end = arr.length;
-    let middle = Math.floor((start + end)/ 2)
+    let end = arr.legnth;
+    let middle = Math.floor((start + end)/ 2);
 
-    while(arr[middle]!==elem && start <= end){
-        if(elem < arr[middle]) end = middle -1;
-        else start = middle + 1;
-        middle = Math.floor((start + end)/2);
-        if(arr[middle] === elem) return middle;
-        return -1;
+    while(arr[middle]!== elem && start < end){
+        if(elem < arr[middle]) end = arr[middle] - 1;
+            else start = arr[middle] + 1;
+            middle = Math.floor((start + end)/2);
     }
+    return arr[middle] === elem ? middle :-1;
 }
