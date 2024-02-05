45070026a4505637562ae94c9d06bf703925c834
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-05 17:10:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function insertionSort(arr){
-	var currentVal;
-    for(var i = 1; i < arr.length; i++){
-        currentVal = arr[i];
-        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-            arr[j+1] = arr[j]
+function naiveSearch(long, short){
+    let count = 0;
+    for(let i = 0; i < long.length; i++){
+        for(let j = 0; j < short.length;j++){
+            if(short[j] !== long[i + j]) break;
+            if(j === short.length - 1) count++;
         }
-        arr[j+1] = currentVal;
     }
-    return arr;
+    return count
 }
-
-insertionSort([2,1,9,76,4])
