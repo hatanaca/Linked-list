812ecfdd5482dc463ae7206fd9f451ff0d081875
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-05 13:09:14 -0300

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr){
+    const swap = (arr, idx1, idx2) =>{
+        [arr[idx1],arr[ídx2]] = [arr[idx2], arr[idx1]];
+    }
+    for(let i = arr.lenth; i > 0; i--){
+        for(let j = 0; j < i - 1;j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr, j, j+1);
+            }
+        }
+    }
+    return arr;
+}
