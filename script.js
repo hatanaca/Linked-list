c3e4e4303a39aaf7700b276b5a1f1b87ede24e87
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-06 15:13:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-function naiveSearch(long, short){
-    let count = 0;
-    for(let i = 0; i < long.length; i++){
-        for(let j = 0; j < short.length;j++){
-            if(short[j] !== long[i + j]) break;
-            if(j === short.length - 1) count++;
+function merge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[2]);
+            j++;
         }
     }
-    return count
+    while(i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
 }
+
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+mergeSort([10,24,76,73])
