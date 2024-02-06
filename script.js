559849a903c6294746f3d2b07e855826522e6953
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-06 15:29:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-function merge(arr1, arr2){
-    let results = [];
-    let i = 0;
-    let j = 0;
-    while(i < arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
-            results.push(arr1[i]);
-            i++;
-        } else {
-            results.push(arr2[2]);
-            j++;
+function bubbleSort(arr){
+    let noSwaps;
+    for(let i = arr.length; i > 0;i--){
+        noSwaps = true;
+        for(let j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j +1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            }
         }
+        if(noSwaps) break;
     }
-    while(i < arr1.length) {
-        results.push(arr1[i]);
-        i++;
-    }
-    while(j < arr2.length) {
-        results.push(arr2[j])
-        j++;
-    }
-    return results;
+    return arr;
 }
-
-function mergeSort(arr){
-    if(arr.length <= 1) return arr;
-    let mid = Math.floor(arr.length/2);
-    let left = mergeSort(arr.slice(0,mid));
-    let right = mergeSort(arr.slice(mid));
-    return merge(left, right);
-}
-mergeSort([15,30,72,5,8,13,7,8,21,55,25,15])
+bubbleSort([8,1,2,3,4,5,6,7]);
