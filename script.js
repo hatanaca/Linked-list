5ca38557558b27894141b13f9e1037a1e69cfe8b
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-02-08 17:20:56 -0300

    Mudando o alortimo Mudando o alortimo

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function bubbleSort(arr){
-    let noSwaps;
-    for(let i = arr.length; i > 0;i--){
-        noSwaps = true;
-        for(let j = 0; j < i - 1; j++){
-            if(arr[j] > arr[j+1]){
-                let temp = arr[j];
-                arr[j] = arr[j +1];
-                arr[j+1] = temp;
-                noSwaps = false;
-            }
-        }
-        if(noSwaps) break;
-    }
-    return arr;
+function pivot(arr, start=0, end=arr.length-1){
+
+  const swap = (arr,idx1,idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2],arr[idx1]]
+  };
+
 }
-bubbleSort([8,1,2,3,4,5,6,7]);
